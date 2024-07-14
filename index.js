c35d78849e3929c2fb20db48c085105589c3d0e4
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-07-14 14:33:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // Створюємо екземпляр додатку Express
 const app = express();
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:3000";
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
