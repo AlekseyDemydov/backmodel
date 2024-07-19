3ed35f2d667b35830331e44e65a14623631e4b58
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-07-19 14:55:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
